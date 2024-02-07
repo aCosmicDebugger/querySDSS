a32591e57da06984371ab09f7db9620e94af0171
aCosmicDebugger <isaac.projetos.123@gmail.com>
2024-02-07 17:50:11 -0300

    v.17

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
     name='querySDSS',
-    version='0.1',
+    version=find_version('querySDSS.py'),
+    description='Query for SDSS datarelease 18',
+    long_description=long_description,
+)
+
+setuptools.setup(
+    name='querySDSS',
+    version='1.7',
     license='MIT',
     author="aCosmicDebbuger",
     author_email='acosmicdebugger@gmail.com',
-    packages=find_packages('src'),
+    packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/aCosmicDebugger/querySDSS',
     keywords='example project',
